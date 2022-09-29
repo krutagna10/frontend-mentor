346573f679301028117e874d485657ba05a16dcf
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-09-29 18:32:08 +0530

    .

--- 09 - Tip Calculator App/Javascript/script.js
+++ 09 - Tip Calculator App/Javascript/script.js
@@ -6,7 +6,10 @@ const billInputElement = document.querySelector('#bill-input');
 const numberOfPersonsElement = document.querySelector('#number-input');
 const tipButtons = document.querySelectorAll('.tip-button');
 const tipValues = document.querySelectorAll('.tip-value');
+const customTipInputElement = document.querySelector('#custom-tip');
 
+
+let currentActive = 2;
 // When the user clicks the reset button
 resetButton.addEventListener('click', () => {
     tipAmountElement.textContent = 0.00.toFixed(2);
@@ -16,20 +19,21 @@ resetButton.addEventListener('click', () => {
     for (let tipButton of tipButtons) {
         tipButton.classList.remove('current-active');
     }
+    currentActive = -1;
     resetButton.disabled = true;
 })
 
+// When the user inputs bill value 
+billInputElement.addEventListener('input', update);
+
+// When the user inputs number of persons value
+numberOfPersonsElement.addEventListener('input', update);
+
+// When the user inputs tip value
 tipButtons.forEach((tipBtn, index) => {
     tipBtn.addEventListener('click', () => {
-        // Declaring variables 
-        let billInput = Number(billInputElement.value);
-        let numberOfPersons = Number(numberOfPersonsElement.value);
-
-        // Enabling the reset button
-        resetButton.disabled = false;
-
+        currentActive = index;
         // Assigning current active class to clicked button and removing current active class from other button.
-        let currentActive = index;
         tipButtons.forEach((tipButton, index) => {
             if (index == currentActive) {
                 tipButton.classList.add('current-active');
@@ -37,16 +41,38 @@ tipButtons.forEach((tipBtn, index) => {
                 tipButton.classList.remove('current-active');
             }
         })
+
+        update();
+    })
+})
+
+// When the user clicks the custom tip Element
+customTipInputElement.addEventListener('input', () => {
+    console.log('Hello');
+    // Remove the current active from 
+})
+
+
+
+// The update function for updating the contents
+function update() {
+    // Declaring variables 
+    let billInput = Number(billInputElement.value);
+    let numberOfPersons = Number(numberOfPersonsElement.value);
+
+    if (numberOfPersons === 0) {
+        // If numbers of persons == 0 then add error class to number of people element
+        document.querySelector('.number-of-people').classList.add('error');
+    } else if (currentActive === -1) {
+        // When the user has not selected any tip
+        document.querySelector('.tip-buttons').classList.add('error');
+    } else {
+        // Remove error class
+        document.querySelector('.number-of-people').classList.remove('error');
+        document.querySelector('.tip-buttons').classList.remove('error');
+
         // Element which has current active class
         let tipValue = document.querySelector('.current-active .tip-value').textContent;
-         
-        // If numbers of persons == 0 then add error class to number of people element
-        if (numberOfPersons == 0) {
-            document.querySelector('.number-of-people').classList.add('error');
-        } else {
-            // Remove error class
-            document.querySelector('.number-of-people').classList.remove('error');
-
         // Tip amount per person
         let totalTipAmount = (tipValue * billInput / 100).toFixed(2);
         tipAmountElement.textContent = (totalTipAmount / numberOfPersons).toFixed(2);
@@ -56,9 +82,8 @@ tipButtons.forEach((tipBtn, index) => {
         //  Total amount per person
         console.log(totalAmount);
         totalAmountElement.textContent = (totalAmount / numberOfPersons).toFixed(2);
-        }
-    })
-})
+    }
+}
 
 
 
--- 09 - Tip Calculator App/temp.js
+++
@@ -1,2 +0,0 @@
-let num = 1000000.2334343;
-console.log(num.toFixed(2));
