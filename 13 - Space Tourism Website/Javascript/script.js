bb12442cd23f62cb2c9da3e966c60a8a54501e39
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-10-09 22:03:37 +0530

    .

--- 13 - Space Tourism Website/Javascript/script.js
+++ 13 - Space Tourism Website/Javascript/script.js
@@ -18,3 +18,16 @@ function update() {
     })
 }
 
+
+// Mobile Navigation
+let menuButton = document.querySelector('.menu-button');
+let closeButton = document.querySelector('.close-button');
+menuButton.addEventListener('click', () => {
+    document.querySelector('.primary-header').classList.add('navigation-open');
+})
+
+closeButton.addEventListener('click', () => {
+    document.querySelector('.primary-header').classList.remove('navigation-open');
+})
+
+
