12242db09fe19f3f5f600494ba49070e125198c7
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-10-18 20:29:49 +0530

    .

--- 15 - Designo Multi Page Website/Javascript/script.js
+++ 15 - Designo Multi Page Website/Javascript/script.js
@@ -1,10 +1,28 @@
 const mainHeader = document.querySelector('.main-header')
 const overlay = document.querySelector('.overlay');
+const webDesignProject = document.querySelector('.web-design');
+const appDesignProject = document.querySelector('.app-design');
+const graphicDesignProject = document.querySelector('.graphic-design');
 
+
+// Navigation
 let mobileNavigationButton = document.querySelector('.mobile-navigation-button');
 
 mobileNavigationButton.addEventListener('click', () => {
     mainHeader.classList.toggle('navigation-open');
     mobileNavigationButton.classList.toggle('active');
     overlay.classList.toggle('hidden');
-})
+});
+
+// When the user clicks on any project container
+webDesignProject.addEventListener('click', () => {
+    window.location.href = 'web-design.html';
+});
+
+appDesignProject.addEventListener('click', () => {
+    window.location.href = 'app-design.html';
+});
+
+graphicDesignProject.addEventListener('click', () => {
+    window.location.href = 'graphic-design.html';
+});
