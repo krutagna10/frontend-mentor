d67cbbcc40615f320e2e6b6e08c63d5759abd156
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-10-22 15:47:26 +0530

    .

--- 16 - Calculator App/Javascript/script.js
+++ 16 - Calculator App/Javascript/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberButtons = document.querySelectorAll('.number-button');
 const operatorButtons = document.querySelectorAll('.operator-button');
 const equalsButton = document.querySelector('.equals-button');
@@ -5,20 +7,15 @@ const outputText = document.querySelector('.output-text');
 const resetButton = document.querySelector('.reset-button');
 const deleteButton = document.querySelector('.delete-button');
 
-let result = 0;
 let operatorIndex = -1;
 let str = '';
 outputText.textContent = '';
 let arr = [];
 
-
 const updateOutputText = () => {
- outputText.textContent = str;
+    outputText.textContent = str;
 }
 
-const displayResult = () => {
-    outputText.textContent = result;
-}
 
 const findOperatorIndex = () => {
     for (const element of str) {
@@ -29,32 +26,34 @@ const findOperatorIndex = () => {
 }
 
 const evaluateExpression = () => {
-    let operand1 = Number(str.slice(0, operatorIndex));
-    let operand2 = Number(str.slice(operatorIndex + 1, str.length));
-    let operatorValue = str[operatorIndex];
+    let operatorValue = str.charAt(operatorIndex);
+    let numbersArray = str.split(operatorValue);
+
+    let operand1 = Number(numbersArray[0]);
+    let operand2 = Number(numbersArray[1]);
 
     if (operatorValue === '+') {
-        result = operand1 + operand2;
+        let result = (operand1 + operand2).toFixed(5);
         str = String(result);
-        displayResult();
+        updateOutputText();
     } else if (operatorValue === '-') {
-        result = operand1 - operand2;
+        let result = (operand1 - operand2).toFixed(5);
         str = String(result);
-        displayResult()
+        updateOutputText()
     } else if (operatorValue === 'x') {
-        result = operand1 * operand2;
+        let result = (operand1 * operand2).toFixed(5);
         str = String(result);
-        displayResult()
+        updateOutputText()
     } else if (operatorValue === '/') {
-        result = operand1 / operand2;
+        let result = (operand1 / operand2).toFixed(5);
         str = String(result);
-        displayResult();
+        updateOutputText();
     }
 }
 
 numberButtons.forEach((numberButton, index) => {
     numberButton.addEventListener('click', () => {
-        let numberValue =  numberButton.textContent;
+        let numberValue = numberButton.textContent;
         str = str + numberValue;
         updateOutputText();
     })
@@ -62,29 +61,38 @@ numberButtons.forEach((numberButton, index) => {
 
 operatorButtons.forEach((operatorButton, index) => {
     operatorButton.addEventListener('click', () => {
-        arr.push(str);
-        findOperatorIndex();
-        if (arr.length >= 2) {
-            evaluateExpression()
-        }
-        let operatorValue = operatorButton.textContent;
-        str = str + operatorValue;
-        updateOutputText();
+        let lastStrCharacter = parseInt(str.charAt(str.length - 1));
 
+        // So that User cannot click on operator two times in a row
+        if (lastStrCharacter || lastStrCharacter === 0) {
+            arr.push(str);
+            findOperatorIndex();
+            if (arr.length >= 2) {
+                evaluateExpression()
+            }
+            let operatorValue = operatorButton.textContent;
+            str = str + operatorValue;
+            updateOutputText();
+
+        }
     })
 })
 
+// Equals Button
+equalsButton.addEventListener('click', () => {
+    findOperatorIndex();
+    evaluateExpression();
+})
+
+
+// Reset Button
 resetButton.addEventListener('click', () => {
     str = '';
     outputText.textContent = str;
 })
 
 
-equalsButton.addEventListener('click', () => {
-    findOperatorIndex();
-    evaluateExpression();
-})
-
+// Deleter Button
 deleteButton.addEventListener('click', () => {
     str = str.replace(str.charAt(str.length - 1), '');
     updateOutputText();
