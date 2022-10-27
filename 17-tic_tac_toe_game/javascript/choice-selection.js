69ab3f6b94dac8b42b381ee651285e6f30af2f2b
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-10-27 10:35:12 +0530

    .

--- 17-tic_tac_toe_game/javascript/choice-selection.js
+++ 17-tic_tac_toe_game/javascript/choice-selection.js
@@ -2,17 +2,21 @@
 
 const markSelectionButtons = document.querySelectorAll('.mark-selection__button');
 const markSelectionIcons = document.querySelectorAll('.mark-selection__icon');
+const newGameButton = document.querySelector('.new-game-button');
+const body = document.querySelector('body');
 
 let userChoice;
+let computerChoice;
 
 
 function updateSelection() {
     markSelectionButtons.forEach((markSelectionButton, index) => {
         if (currentChoiceIndex === index) {
             markSelectionButton.classList.add('mark-selection__active-button');
-            markSelectionIcons[index].src = 'images/icon-'
+            markSelectionIcons[index].src = `images/icon-${userChoice}-dark-navy.svg`;
         } else {
             markSelectionButton.classList.remove("mark-selection__active-button");
+            markSelectionIcons[index].src = `images/icon-${computerChoice}-silver.svg`;
         }
     })
 }
@@ -20,8 +24,19 @@ function updateSelection() {
 let currentChoiceIndex = -1;
 markSelectionButtons.forEach((markSelectionButton, index) => {
     markSelectionButton.addEventListener('click', () => {
+        if (markSelectionButton.classList.contains('x-selection')) {
+            userChoice = 'x';
+            computerChoice = 'o';
+        }  else {
+            userChoice = 'o';
+            computerChoice = 'x';
+        }
         currentChoiceIndex = index;
         updateSelection();
     })
 })
 
+
+newGameButton.addEventListener('click', () => {
+    body.classList.add('game-active');
+})
