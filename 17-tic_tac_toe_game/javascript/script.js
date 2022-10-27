b121ca9dab2ab32fddc363b4a2eda5c56ac398f6
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-10-27 09:48:04 +0530

    .

--- 17-tic_tac_toe_game/javascript/script.js
+++ 17-tic_tac_toe_game/javascript/script.js
@@ -1,4 +1,27 @@
 'use strict';
 
-const gameContainer = document.querySelector('.game-container');
+const markSelectionButtons = document.querySelectorAll('.mark-selection__button');
+const markSelectionIcons = document.querySelectorAll('.mark-selection__icon');
+
+let userChoice;
+
+
+function updateSelection() {
+    markSelectionButtons.forEach((markSelectionButton, index) => {
+        if (currentChoiceIndex === index) {
+            markSelectionButton.classList.add('mark-selection__active-button');
+            markSelectionIcons[index].src = 'images/icon-'
+        } else {
+            markSelectionButton.classList.remove("mark-selection__active-button");
+        }
+    })
+}
+
+let currentChoiceIndex = -1;
+markSelectionButtons.forEach((markSelectionButton, index) => {
+    markSelectionButton.addEventListener('click', () => {
+        currentChoiceIndex = index;
+        updateSelection();
+    })
+})
 
