8e090da7c47f325a90a2e0b26424aaf8fe90a8bb
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-10-27 13:01:42 +0530

    .

--- 17-tic_tac_toe_game/javascript/script.js
+++ 17-tic_tac_toe_game/javascript/script.js
@@ -5,9 +5,8 @@ const markSelectionIcons = document.querySelectorAll('.mark-selection__icon');
 const newGameButton = document.querySelector('.new-game-button');
 const body = document.querySelector('body');
 
-let userChoice;
-let computerChoice;
-
+let userChoice = 'o';
+let computerChoice = 'x';
 
 function updateSelection() {
     markSelectionButtons.forEach((markSelectionButton, index) => {
@@ -27,7 +26,7 @@ markSelectionButtons.forEach((markSelectionButton, index) => {
         if (markSelectionButton.classList.contains('x-selection')) {
             userChoice = 'x';
             computerChoice = 'o';
-        }  else {
+        } else {
             userChoice = 'o';
             computerChoice = 'x';
         }
@@ -40,3 +39,38 @@ markSelectionButtons.forEach((markSelectionButton, index) => {
 newGameButton.addEventListener('click', () => {
     body.classList.add('game-active');
 })
+
+let options = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+const gameWrapper = document.querySelector('.game__wrapper');
+const choiceButtons = document.querySelectorAll('.choice-button');
+
+const pickComputerChoice = () => {
+    let computerChoiceImage = document.createElement('img');
+    computerChoiceImage.src = `images/icon-${computerChoice}.svg`;
+    let random = options[parseInt(Math.random() * (options.length))];
+    choiceButtons[random].appendChild(computerChoiceImage);
+    choiceButtons[random].disabled = true;
+
+    let arrayIndex = options.findIndex((element) => element === random);
+    options.splice(arrayIndex, 1);
+    console.log(options);
+
+}
+
+choiceButtons.forEach((choiceButton, index) => {
+    choiceButton.addEventListener('click', () => {
+        let image = document.createElement('img');
+        image.src = `images/icon-${userChoice}.svg`;
+        choiceButton.appendChild(image);
+        choiceButton.disabled = true;
+
+        let arrayIndex = options.findIndex((element) => element === index);
+        options.splice(arrayIndex, 1);
+        console.log(options);
+
+        pickComputerChoice();
+    })
+});
+
+
