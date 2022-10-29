92b2765fee3fc6bc55c30817b2858639cb4576e8
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-10-29 17:34:49 +0530

    .

--- 17-tic_tac_toe_game/javascript/practice.js
+++
@@ -1,7 +0,0 @@
-const winArray = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-let arr = [0, 1, 2];
-
-
-for (const element of winArray) {
-    console.log(element.every(element => arr.includes(element)));
-}
--- 17-tic_tac_toe_game/javascript/script.js
+++ 17-tic_tac_toe_game/javascript/script.js
@@ -27,7 +27,6 @@ let availableChoices = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 const winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-
 // Elements
 const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
