450982dfe2301bb2483e98f56c4b7088bdbe1598
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-10-30 21:06:57 +0530

    .

--- 06-3_column_preview_card_component/script.js
+++
@@ -1,7 +0,0 @@
-const icons = document.querySelectorAll('.icon');
-let currentActive = -1;
-for (let icon of icons) {
-    icon.addEventListener('click', () => {
-        icon.classList.toggle('active');
-    })
-}
