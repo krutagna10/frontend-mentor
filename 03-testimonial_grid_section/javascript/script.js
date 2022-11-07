52932488070717390612db3b39596e0e15c80d32
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-11-07 11:14:59 +0530

    .

--- 03-testimonial_grid_section/javascript/script.js
+++ 03-testimonial_grid_section/javascript/script.js
@@ -1,12 +1,12 @@
-const lightModeIcon = document.querySelector('.light-mode-icon');
-const darkModeIcon = document.querySelector('.dark-mode-icon');
+const lightModeIcon = document.querySelector('.icon__light-mode');
+const darkModeIcon = document.querySelector('.icon__dark-mode');
 const body = document.querySelector('body');
 
 darkModeIcon.addEventListener('click', () => {
-    body.classList.add('dark-mode');
+    body.classList.add('dark-mode--active');
 })
 
 lightModeIcon.addEventListener('click', () => {
-    body.classList.remove('dark-mode')
+    body.classList.remove('dark-mode--active')
 })
 
